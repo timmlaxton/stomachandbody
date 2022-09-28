cef06e9936606b4a8d6d7915670b955128d83a53
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-09-28 06:07:47 +0100

    :

---
+++ script.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector("nav svg");
+
+burger.addEventListener("click", () => {
+  if (burger.classList.contains("active")) {
+    gsap.to(".links", { x: "100%" });
+  } else {
+    gsap.to(".links", { x: "0%" });
+  }
+  burger.classList.toggle("active");
+});
