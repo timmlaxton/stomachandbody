5abc59a073164e703c496a49b56058dd1312b02e
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-09-28 06:10:20 +0100

    burger bar renders on menu

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const burger = document.querySelector("nav svg");
 burger.addEventListener("click", () => {
   if (burger.classList.contains("active")) {
     gsap.to(".links", { x: "100%" });
+    gsap.to(".line", { stroke: "white" });
   } else {
     gsap.to(".links", { x: "0%" });
+    gsap.to(".line", { stroke: "black" });
   }
   burger.classList.toggle("active");
 });
