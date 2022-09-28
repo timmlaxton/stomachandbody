3a1ca9c17a5d6edf53961f2c5200bd6a09d36146
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2022-09-28 06:22:59 +0100

    aded scroll to images

--- script.js
+++ script.js
@@ -4,9 +4,32 @@ burger.addEventListener("click", () => {
   if (burger.classList.contains("active")) {
     gsap.to(".links", { x: "100%" });
     gsap.to(".line", { stroke: "white" });
+    gsap.set("body", { overflow: "auto" });
+    gsap.set("body", { overflowX: "hidden" });
   } else {
     gsap.to(".links", { x: "0%" });
     gsap.to(".line", { stroke: "black" });
+    gsap.fromTo(
+      ".links a",
+      { opacity: 0 },
+      { opacity: 1, delay: 0.5, stagger: 0.67 }
+    );
+    gsap.set("body", { overflow: "hidden" });
   }
   burger.classList.toggle("active");
+});
+
+const videos = gsap.utils.toArray(".video");
+gsap.set(videos, { opacity: 0 });
+videos.forEach((video) => {
+  ScrollTrigger.create({
+    trigger: video,
+    start: "top, center",
+    end: "bottom, center",
+    markers: true,
+    onEnter: () => {
+      gsap.to(video, { opacity: 1 });
+      video.play();
+    },
+  });
 });
